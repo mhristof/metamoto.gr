026fe8d14998c05c4e42fa2306c7f60753674ab9
Mike Christofilopoulos <mhristof@gmail.com>
2025-02-21 10:53:03 +0200

    fix(scrape): run every date at 4am, optimize tables after each run

---
+++ scrape.entrypoint.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+set -x
+
+echo "Starting scrape.entrypoint.sh"
+
+# run every day at 4am Europe/Athens time
+while true; do
+    now=$(TZ="Europe/Athens" date +%H%M)
+    if [[ "$now" == "0400" ]]; then
+        echo "Running scrape.sh"
+        python scrape/*.py
+        for table in $(clickhouse client --host clickhouse -q 'show tables'); do
+            clickhouse client --host clickhouse -q "optimize table $table final"
+        done
+    fi
+    echo "Now: $now, sleeping for 30 seconds"
+    sleep 30
+done
