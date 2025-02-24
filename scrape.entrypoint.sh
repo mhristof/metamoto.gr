a2cc29fb6f799ee8ceae63d1efa4e5fd930711a4
Mike Christofilopoulos <mhristof@gmail.com>
2025-02-24 14:55:54 +0200

    fix(scrape): find the correct scrape scripts

--- scrape.entrypoint.sh
+++ scrape.entrypoint.sh
@@ -2,8 +2,6 @@
 
 set -euo pipefail
 
-set -x
-
 echo "Starting scrape.entrypoint.sh"
 
 # run every day at 4am Europe/Athens time
@@ -11,7 +9,7 @@ while true; do
     now=$(TZ="Europe/Athens" date +%H%M)
     if [[ "$now" == "0400" ]]; then
         echo "Running scrape.sh"
-        python scrape/*.py
+        find /app/scrape -name '*.py' -exec python {} \;
         for table in $(clickhouse client --host clickhouse -q 'show tables'); do
             clickhouse client --host clickhouse -q "optimize table $table final"
         done
