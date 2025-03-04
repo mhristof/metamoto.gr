24ae9af3f321c2251289a00980924da04434f37a
Mike Christofilopoulos <mhristof@gmail.com>
2025-03-04 11:43:01 +0200

    fix(scrape): remove find util to run all scrapers

--- scrape.entrypoint.sh
+++ scrape.entrypoint.sh
@@ -2,14 +2,20 @@
 
 set -euo pipefail
 
-echo "Starting scrape.entrypoint.sh"
+SCRIPT=${1:-}
+
+if [[ -z $SCRIPT ]]; then
+    echo "Usage: $0 <script>"
+    exit 1
+fi
 
 # run every day at 4am Europe/Athens time
 while true; do
     now=$(TZ="Europe/Athens" date +%H%M)
     if [[ "$now" == "0400" ]]; then
-        echo "Running scrape.sh"
-        find /app/scrape -name '*.py' -exec python {} \;
+        echo "Running $SCRIPT"
+        python "$SCRIPT"
+
         for table in $(clickhouse client --host clickhouse -q 'show tables'); do
             clickhouse client --host clickhouse -q "optimize table $table final"
         done
